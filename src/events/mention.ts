e504327950a8439f44f53cc4c274d02ecfde1137
carterdacat <56525188+carterdacat@users.noreply.github.com>
2023-11-19 19:46:53 -0500

    rebases

--- src/events/mention.ts
+++ src/events/mention.ts
@@ -1,18 +1,26 @@
 import Bot from "../main/Bot"
-
+import coinflip from "coinflip"
 module.exports = class mention {
     client: Bot
     constructor(client: Bot) {
         this.client = client
     }
     async run(data) {
-        console.log(data)
-            //GET GROUP
-            //https://web.realsports.io/comments/groups/16494/replies/4621013?limit=10
-            //GET COMMENT POST
-            //https://web.realsports.io/comments/posts/99313/replies/5014904?limit=10   
-            //REPLY (POST)
-            //https://web.realsports.io/comments/posts/99313
-            // Make these "Bot" functions where you go client.getComment("99313") => commentData
+        // Extract the number from the path property
+        const path = data[1].path;
+        const match = path.match(/\/(\d+)$/);
+        // If a match is found, extract the number
+        const commentId = match ? match[1] : null;
+        const entity = await (await this.client.getActivity(commentId)).getEntity()
+        this.client.logger.log(`Command ran by ${entity.user.userName}!`, "log")
+        const reply = entity.reply(coinflip() ? "Yes!" : "No!")
+        //GET GROUP
+        //https://web.realsports.io/comments/groups/16494/replies/4621013?limit=10
+        //GET COMMENT POST
+        //https://web.realsports.io/comments/posts/99313/replies/5014904?limit=10
+        //REPLY (POST)
+        //https://web.realsports.io/comments/posts/99313
+        // Make these "Bot" functions where you go client.getComment("99313") => commentData
     }
 }
+
--- src/utils/getPost.ts
+++ src/utils/getPost.ts
@@ -1,19 +1,21 @@
 import axios from "axios";
 import Bot from "../main/Bot";
 import Post from "../classes/Post";
+import Entitiy from "../classes/Entity";
+import Entity from "../classes/Entity";
 
 export default async function getPostFunction(
     client: Bot,
     postId: number,
-    commentId: number
-): Promise<Post> {
+    commentId: number | string
+): Promise<Entity> {
     try {
         const res = await axios.get(
             `https://web.realsports.io/comments/posts/${postId}/replies/${commentId}?limit=10`,
             {
                 method: "get",
                 headers: {
-                    "real-auth-info": "KnA6mEEJ!Y35g7vRG!801a60f9-9b0e-4811-b4f7-82cd53442a7b",
+                    "real-auth-info": process.env.auth,
                     "real-device-uuid": "178ae57e-6575-4119-bb26-9b7102fd5b69",
                     "real-device-type": "desktop_web",
                     "real-device-token": "token",
@@ -21,7 +23,7 @@ export default async function getPostFunction(
                 },
             }
         );
-        return new Post(res.data, client);
+        return new Entity(res.data, client);
     } catch (error) {
         // Handle errors if necessary
         throw error;
